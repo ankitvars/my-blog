6d50348660a024897c750b1bb75d7d1f88b6a926
Ankit Varshney <varshneyankit011@gmail.com>
2023-01-11 15:44:09 +0530

    Finally all styling is completed only global style is remain in index.css file

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,27 @@ import PostDetail from './PostDetail';
 import CreatePost from './CreatePost';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  width: 70vw;
+  margin-left: auto;
+  margin-right: auto;
+  margin: 0 auto;
+  box-sizing: border-box;
+`;
 
 function App() {
   return (
     <div className="app">
       <Navbar />
-      <div className="container">
+      <Div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/post/:postId" element={<PostDetail />} />
           <Route path="/create-post" element={<CreatePost />} />
         </Routes>
-      </div>
+      </Div>
       <Footer />
     </div>
   );
